4847a97404a919fcef6a8d252f0f93eaedacd4b3
artggd <arthur@guigand.com>
2023-12-02 21:32:15 +0100

    use native macos accents with azerty keyboard

--- config/boards/shields/microdix/helper/keymap_french.h
+++ config/boards/shields/microdix/helper/keymap_french.h
@@ -1,30 +1,30 @@
 /*
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
- * │ ² │ & │ é │ " │ ' │ ( │ - │ è │ _ │ ç │ à │ ) │ = │       │
+ * │ @ │ & │ é │ " │ ' │ ( │ § │ è │ ! │ ç │ à │ ) │ - │       │
  * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
  * │     │ A │ Z │ E │ R │ T │ Y │ U │ I │ O │ P │ ^ │ $ │     │
  * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┐    │
- * │      │ Q │ S │ D │ F │ G │ H │ J │ K │ L │ M │ ù │ * │    │
+ * │      │ Q │ S │ D │ F │ G │ H │ J │ K │ L │ M │ ù │ ` │    │
  * ├────┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴────┤
- * │    │ < │ W │ X │ C │ V │ B │ N │ , │ ; │ : │ ! │          │
+ * │    │ < │ W │ X │ C │ V │ B │ N │ , │ ; │ : │ = │          │
  * ├────┼───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤
  * │    │    │    │                        │    │    │    │    │
  * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
  */
 // Row 1
-#define FR_SUP2 GRAVE       // ²
+//#define FR_SUP2 GRAVE       // ²
 #define FR_AMPR N1          // &
 #define FR_EACU N2          // é
 #define FR_DQUO N3          // "
 #define FR_QUOT N4          // '
 #define FR_LPRN N5          // (
-#define FR_MINS N6          // -
+#define FR_SECT N6          // §
 #define FR_EGRV N7          // è
-#define FR_UNDS N8          // _
+#define FR_EXLM N8          // !
 #define FR_CCED N9          // ç
 #define FR_AGRV N0          // à
 #define FR_RPRN MINUS       // )
-#define FR_EQL  EQUAL       // =
+#define FR_MINS EQUAL       // -
 // Row 2
 #define FR_A    Q           // A
 #define FR_Z    W           // Z
@@ -50,7 +50,7 @@
 #define FR_L    L           // L
 #define FR_M    SEMI        // M
 #define FR_UGRV SQT         // ù
-#define FR_ASTR NON_US_HASH // *
+#define FR_GRV  NON_US_BACKSLASH // `
 // Row 4
 #define FR_LABK NON_US_BSLH // <
 #define FR_W    Z           // W
@@ -62,17 +62,17 @@
 #define FR_COMM M           // ,
 #define FR_SCLN COMMA       // ;
 #define FR_COLN DOT         // :
-#define FR_EXLM FSLH        // !
+#define FR_EQL FSLH         // =
 
 /* Shifted symbols
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
- * │   │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │ ° │ + │       │
+ * │   │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │ ° │ _ │       │
  * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
- * │     │   │   │   │   │   │   │   │   │   │   │ ¨ │ £ │     │
+ * │     │   │   │   │   │   │   │   │   │   │   │ ¨ │ * │     │
  * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┐    │
- * │      │   │   │   │   │   │   │   │   │   │   │ % │ µ │    │
+ * │      │   │   │   │   │   │   │   │   │   │   │ % │ £ │    │
  * ├────┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴────┤
- * │    │ > │   │   │   │   │   │   │ ? │ . │ / │ § │          │
+ * │    │ > │   │   │   │   │   │   │ ? │ . │ / │ + │          │
  * ├────┼───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤
  * │    │    │    │                        │    │    │    │    │
  * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
@@ -83,25 +83,25 @@
 #define FR_3    LS(FR_DQUO) // 3
 #define FR_4    LS(FR_QUOT) // 4
 #define FR_5    LS(FR_LPRN) // 5
-#define FR_6    LS(FR_MINS) // 6
+#define FR_6    LS(FR_SECT) // 6
 #define FR_7    LS(FR_EGRV) // 7
 #define FR_8    LS(FR_UNDS) // 8
 #define FR_9    LS(FR_CCED) // 9
 #define FR_0    LS(FR_AGRV) // 0
 #define FR_DEG  LS(FR_RPRN) // °
-#define FR_PLUS LS(FR_EQL)  // +
+#define FR_UNDS LS(FR_EQL)  // _
 // Row 2
 #define FR_DIAE LS(FR_CIRC) // ¨ (dead)
-#define FR_PND  LS(FR_DLR)  // £
+#define FR_ASTR LS(FR_DLR)  // *
 // Row 3
 #define FR_PERC LS(FR_UGRV) // %
-#define FR_MICR LS(FR_ASTR) // µ
+#define FR_PND LS(FR_GRV)   // £
 // Row 4
 #define FR_RABK LS(FR_LABK) // >
 #define FR_QUES LS(FR_COMM) // ?
 #define FR_DOT  LS(FR_SCLN) // .
 #define FR_SLSH LS(FR_COLN) // /
-#define FR_SECT LS(FR_EXLM) // §
+#define FR_PLUS LS(FR_EQL)  // +
 
 /* AltGr symbols
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
@@ -117,16 +117,15 @@
  * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
  */
 // Row 1
-#define FR_TILD RA(FR_EACU) // ~ (dead)
-#define FR_HASH RA(FR_DQUO) // #
-#define FR_LCBR RA(FR_QUOT) // {
-#define FR_LBRC RA(FR_LPRN) // [
-#define FR_PIPE RA(FR_MINS) // |
-#define FR_GRV  RA(FR_EGRV) // ` (dead)
-#define FR_BSLS RA(FR_UNDS) // (backslash)
-#define FR_AT   RA(FR_AGRV) // @
-#define FR_RBRC RA(FR_RPRN) // ]
-#define FR_RCBR RA(FR_EQL)  // }
+#define FR_TILD RA(FR_N)        // ~ (dead)
+#define FR_HASH NUHS            // #
+#define FR_LCBR RA(FR_LPRN)     // {
+#define FR_LBRC RA(RS(FR_LPRN)) // [
+#define FR_PIPE RA(RS(FR_L))    // |
+#define FR_BSLS RA(RS(FR_COLN)) // (backslash)
+#define FR_AT   RA(FR_AGRV)     // @
+#define FR_RBRC RA(RS(FR_RPRN)) // ]
+#define FR_RCBR RA(FR_RPRN)     // }
 // Row 2
-#define FR_EURO RA(E)   // €
-#define FR_CURR RA(FR_DLR) // ¤
+#define FR_EURO RA(FR_DLR)   // €
+
